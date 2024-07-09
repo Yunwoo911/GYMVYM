ee91a550df5eadd42a0969db2386d44ad5a2c28d
SinMoonGh <109773691+SinMoonGh@users.noreply.github.com>
2024-07-09 11:56:19 +0900

    done

--- account/models.py
+++ account/models.py
@@ -20,79 +20,11 @@ class CustomUser(models.Model):
     birth = models.DateField()
     gender = models.CharField(max_length=10)
 
+
 class Owner(models.Model):
     owner_id = models.AutoField(primary_key=True)
     owner_name = models.CharField(max_length=100)
-
-class Gym(models.Model):
-    gym_id = models.AutoField(primary_key=True)
-    owner = models.ForeignKey(Owner, on_delete=models.CASCADE)
-    gym_name = models.CharField(max_length=100)
-    gym_address = models.CharField(max_length=255)
-
-class Trainer(models.Model):
-    trainer_id = models.AutoField(primary_key=True)
-    gym = models.ForeignKey(Gym, on_delete=models.CASCADE)
-    owner = models.ForeignKey(Owner, on_delete=models.CASCADE)
-    trainer_name = models.CharField(max_length=100)
-    certificate = models.CharField(max_length=100, null=True)
-    trainer_image = models.ImageField(upload_to='trainer_images', null=True)
-
-class GymMember(models.Model):
-    member_id = models.AutoField(primary_key=True)
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
-    gym = models.ForeignKey(Gym, on_delete=models.CASCADE)
-    join_date = models.DateField()
-    membership_type = models.CharField(max_length=100)
-    expiry_date = models.DateField()
-    recent_joined_date = models.DateField()
-    recent_membership = models.CharField(max_length=100)
-    recent_expiry = models.DateField()
-    renewal_status = models.BooleanField(default=False)
-    renewal_count = models.IntegerField(default=0)
-
-class PT(models.Model):
-    pt_id = models.AutoField(primary_key=True)
-    member = models.ForeignKey(GymMember, on_delete=models.CASCADE)
-    trainer = models.ForeignKey(Trainer, on_delete=models.CASCADE)
-    member_name = models.CharField(max_length=100)
-    registration_date = models.DateField()
-    pt_end_date = models.DateField()
-    duration = models.IntegerField()
-
-class Equipment(models.Model): # 헬스장 기구
-    equipment_id = models.AutoField(primary_key=True)
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, null=True) #사용자
-    equipment_name = models.CharField(max_length=100) #이름
-
-class EquipmentReservation(models.Model): # 기구 예약
-    res_id = models.AutoField(primary_key=True)
-    equipment = models.ForeignKey(Equipment, on_delete=models.CASCADE) # 기구
-    start_time = models.DateTimeField(null=True) # 시작 시간
-    end_time = models.DateTimeField(null=True) # 종료 시간
-
-class EquipmentInUse(models.Model): # 사용중인 기구
-    use_equip_id = models.AutoField(primary_key=True)
-    equipment = models.ForeignKey(Equipment, on_delete=models.CASCADE)
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
-    start_time = models.DateTimeField(null=True)
-    end_time = models.DateTimeField(null=True)
-
-class VisitLog(models.Model): # 출입관리
-    visitlog_id = models.AutoField(primary_key=True)
-    member = models.ForeignKey(GymMember, on_delete=models.CASCADE)
-    nfc_uid = models.CharField(max_length=30, null=True)
-    enter_time = models.DateTimeField()
-    exit_time = models.DateTimeField(null=True)
-    fields = models.CharField(max_length=100, null=True)
-
-class DataAnalysis(models.Model):
-    analysis_id = models.AutoField(primary_key=True)
-    equipment = models.ForeignKey(Equipment, on_delete=models.CASCADE)
-    equipment_use_time = models.DateTimeField()
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
-    member = models.ForeignKey(GymMember, on_delete=models.CASCADE)
-    fields = models.CharField(max_length=100, null=True)
+    
 
 # 채팅방
 class ChatRoom(models.Model):
--- equipment/models.py
+++ equipment/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from account import CustomUser
 
 # Create your models here.
+class Equipment(models.Model): # 헬스장 기구
+    equipment_id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, null=True) #사용자
+    equipment_name = models.CharField(max_length=100) #이름
+
+class EquipmentReservation(models.Model): # 기구 예약
+    res_id = models.AutoField(primary_key=True)
+    equipment = models.ForeignKey(Equipment, on_delete=models.CASCADE) # 기구
+    start_time = models.DateTimeField(null=True) # 시작 시간
+    end_time = models.DateTimeField(null=True) # 종료 시간
+
+class EquipmentInUse(models.Model): # 사용중인 기구
+    use_equip_id = models.AutoField(primary_key=True)
+    equipment = models.ForeignKey(Equipment, on_delete=models.CASCADE)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    start_time = models.DateTimeField(null=True)
+    end_time = models.DateTimeField(null=True)
