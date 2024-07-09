d45f820f14d6694e2df8ba1653be786468118d78
goldangeee <goldangeee@goldangeeeui-noteubug.local>
2024-07-09 22:47:01 +0900

    code review test f

--- chat/models.py
+++ chat/models.py
@@ -21,3 +21,4 @@ class Message(models.Model):
     content = models.TextField() # 메시지 내용
     created_at = models.DateTimeField(auto_now_add=True) # 메시지 생성 시간
     is_read = models.BooleanField(default=False) # 읽음 여부 확인
+# test
