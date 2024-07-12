793123eb096bc2119e15514ab1bef904fb84ecbd
SinMoonGh <109773691+SinMoonGh@users.noreply.github.com>
2024-07-12 17:39:31 +0900

    gyms 모델 약간 수정했습니다. 트레이너  pt 회원 정보 구현중...#

--- gyms/urls.py
+++ gyms/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import TrainerPageView
+from .views import TrainerPageView, TrainerDetailPageView, ProfileAddPageView, TrainerPortfolioView
 
 urlpatterns = [
-    path('trainer/', TrainerPageView.as_view(), name='trainer_page'),
+    path('trainer/PT_membership_management/profile/', TrainerPageView.as_view(), name='trainer_page'),
+    path('trainer/PT_membership_management/profile/user_num/<int:id>/', TrainerDetailPageView.as_view(), name='trainer_detail_page'),
+    path('trainer/PT_membership_management/profile/user_num/<int:id>/profile_add/', ProfileAddPageView.as_view(), name='profile_add_page'),
+    # path('trainer/portflio/trainer_num/<int:id>/', TrainerPortfolioView.as_view(), name='trainer_portflio_page'),
 ]
--- gyms/views.py
+++ gyms/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
-from .models import GymMember
-
-
+from .models import GymMember, PersonalInfo, Trainer
+from gyms.forms import PersonalInfoForm
+from account.models import CustomUser
+import random
 
 # Create your views here.
 class TrainerPageView(TemplateView):
@@ -12,3 +13,50 @@ class TrainerPageView(TemplateView):
         context = super().get_context_data(**kwargs)
         context['gym_members'] = GymMember.objects.all()
         return context
+    
+
+class TrainerDetailPageView(TemplateView):
+    template_name = 'trainer_detail_page.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        gym_member_id = self.kwargs.get('id')
+        # context['gym_members'] = GymMember.objects.all()
+        context['personal_info'] = PersonalInfo.objects.filter(gym_member_if__member_id=gym_member_id)
+        return context
+    
+
+class ProfileAddPageView(TemplateView):
+    template_name = 'profile_add_page.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        gym_member_id = self.kwargs.get('id')
+        context['form'] = PersonalInfoForm()
+        return context
+
+    # def post(self, request, *args, **kwargs):
+    #     form = PersonalInfoForm(request.POST)
+    #     if form.is_valid():
+    #         personal_info = form.save(commit=False)
+    #         gym_member_id = self.kwargs.get('id')
+    #         personal_info.gym_member_if = GymMember.objects.get(member_id=gym_member_id)
+    #         personal_info.save()
+    #         return render(request, 'profile_add_success.html', {'form': form})
+    #     return render(request, self.template_name, {'form': form})    
+
+
+class TrainerPortfolioView(TemplateView):
+    template_name = 'trainer_portfolio_page.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)        
+        # user = self.kwargs.get('id')
+        user = 15
+        context['trainer'] = CustomUser.objects.filter(user = user)
+        # context['trainer'] = Trainer.objects.filter(user = user)
+        return context
+        
+
+
+
