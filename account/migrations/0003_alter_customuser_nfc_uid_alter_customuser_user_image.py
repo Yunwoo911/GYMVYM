aa1973e2a7d6502e7eee180770e1fd008cfb3b02
SinMoonGh <109773691+SinMoonGh@users.noreply.github.com>
2024-07-17 17:39:31 +0900

    마이그레이션 account 3번 파일#

--- account/migrations/0003_alter_customuser_nfc_uid_alter_customuser_user_image.py
+++ account/migrations/0003_alter_customuser_nfc_uid_alter_customuser_user_image.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('account', '0002_alter_customuser_usertype'),
+        ('account', '0002_alter_customuser_user_image'),
     ]
 
     operations = [
