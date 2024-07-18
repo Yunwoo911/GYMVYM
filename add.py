0dd0f47e7a70885fa8aad0b5dbab6ccd25f877b5
yeojin <0929.y.jin@gmail.com>
2024-07-18 10:33:47 +0900

    feat: 이름 중복처리 기능 추가

--- add.py
+++ add.py
@@ -6,6 +6,12 @@ from add_DB import CRUD
 reader = SimpleMFRC522()
 db = CRUD()  # CRUD 객체 생성
 
+# 이름으로 검색
+def search(name):
+    result = db.readDB(name)
+    print("정보 : ", result)
+    return result
+
 # NFC 카드를 읽는 기능
 def read_with_timeout(timeout=1):
     start_time = time.time()
@@ -17,6 +23,15 @@ def read_with_timeout(timeout=1):
             return None, None
         time.sleep(0.1)
 
+# 이름으로 사용자 선택
+def select_user(users):
+    print("여러 명의 사용자가 검색되었습니다. 업데이트할 사용자를 선택하세요.")
+    for i, user in enumerate(users):
+        print(f"{i}: {user}")
+    index = int(input("업데이트할 사용자의 인덱스를 입력하세요: "))
+    selected_user = users[index]
+    return selected_user
+
 # NFC 카드를 읽고, 읽은 ID와 사용자 입력 username을 데이터베이스에 저장
 def add_to_database():
     try:
@@ -24,26 +39,50 @@ def add_to_database():
         if id is not None:
             # 사용자의 username 입력 받기
             username = input("등록하실 분의 이름을 적어주세요: ")
-            
             # 데이터베이스에서 username 조회
-            user_data = db.readDB(username)
-            
-            if not user_data:
+            users = search(username)
+            if not users:
                 print("없는 회원입니다.")
             else:
+                # 여러 명의 사용자가 검색되면 선택
+                if len(users) > 1:
+                    selected_user = select_user(users)
+                else:
+                    selected_user = users[0]
+
                 # 데이터베이스에서 nfc_uid 조회
                 nfc_data = db.read_by_nfc_uid(str(id))
-                
                 if nfc_data:
                     print("이미 존재하는 NFC ID입니다.")
                 else:
                     # 데이터베이스에 NFC ID와 사용자 입력 username 저장
-                    db.updateNFC(nfc_uid=str(id), username=username)
+                    db.updateNFC(nfc_uid=str(id), username=selected_user[0])
                     print("NFC ID {} 이(가) 데이터베이스에 저장되었습니다.".format(str(id)))
         else:
             print("Timeout: NFC 카드를 인식할 수 없습니다.")
     finally:
         GPIO.cleanup()
 
+# 인터페이스 함수
+def interface():
+    while True:
+        name_to_search = input("검색할 사용자의 이름을 입력하세요: ")
+        if not name_to_search: # 빈칸으로 입력했을 때
+            print("이름을 입력해 주세요.")
+            continue
+        users = search(name_to_search)
+        
+        if not users: # 이름이 틀렸을 때
+            print("사용자를 찾을 수 없습니다. 다시 시도해 주세요.")
+            continue
+        
+        new_nfc_uid = input("새로운 NFC UID를 입력하세요: ")
+        
+        if not new_nfc_uid: # 빈칸으로 입력했을 때
+            print("NFC UID를 입력해 주세요.")
+            continue
+        if add_to_database():
+            break
+
 if __name__ == "__main__":
-    add_to_database()
+    interface()
