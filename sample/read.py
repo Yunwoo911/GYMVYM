35efc8994bae107482ced74424073fe93b09cd68
yeojin <0929.y.jin@gmail.com>
2024-07-25 11:36:43 +0900

    feat: 출입 기록 체크

---
+++ sample/read.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import RPi.GPIO as GPIO
+from mfrc522 import SimpleMFRC522
+
+reader = SimpleMFRC522()
+
+try:
+        id, text = reader.read()
+        print(id)
+        print(text)
+finally:
+        GPIO.cleanup()
--- visitlog/visitlog.py
+++
@@ -1,7 +0,0 @@
-import RPi.GPIO as GPIO
-from mfrc522 import SimpleMFRC522
-import time
-from visitlog.visit_DB import CRUD
-
-reader = SimpleMFRC522()
-db = CRUD()  # CRUD 객체 생성
