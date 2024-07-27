fcc1a6528ed3cbebff2db96967dfe1110398d8c1
yeojin-0 <0929.y.jin@gmail.com>
2024-07-28 04:32:58 +0900

    remove: 필요없는 파일 삭제

--- visitlog.py
+++ visitlog.py
@@ -7,7 +7,8 @@ GYM_ID = 1
 
 reader = SimpleMFRC522()
 
-DJANGO_ENDPOINT = "https://a146-122-34-248-34.ngrok-free.app/visitlogs/nfc_entrance/"  # Django 서버의 실제 URL로 변경
+DJANGO_ENDPOINT = "https://4c19-122-34-248-34.ngrok-free.app/visitlogs/identify_nfc/"
+#DJANGO_ENDPOINT = "https://a146-122-34-248-34.ngrok-free.app/visitlogs/nfc_entrance/"  # Django 서버의 실제 URL로 변경
 
 def send_to_django(nfc_uid, gym_id):
     print(f"전송할 NFC UID: {nfc_uid}, gym_id: {gym_id}")  # 디버깅 출력 추가
@@ -20,21 +21,23 @@ def send_to_django(nfc_uid, gym_id):
             print("Django 서버에 NFC UID를 성공적으로 전송했습니다.")
         else:
             print(f"NFC UID 전송에 실패했습니다. 상태 코드: {response.status_code}")
-            print(f"서버 응답 내용: {response.text}")  # 서버 응답 내용 추가
+            #print(f"서버 응답 내용: {response.text}")  # 서버 응답 내용 추가
     except requests.RequestException as e:
         print(f"Django에 데이터를 전송하는 중 오류가 발생했습니다: {e}")
 
 
-
-try:
-    print("리더기 근처에 태그를 가져다 대세요.")
-    while True:
+def interface():
+    try:
+        print("리더기 근처에 태그를 가져다 대세요.")
         id, text = reader.read()
         id_str = str(id)  # NFC UID를 문자열로 변환
         print(f"ID: {id_str}")
         send_to_django(id_str, GYM_ID)  # 문자열로 전송
         time.sleep(2)  # 연속 읽기 방지를 위한 대기 시간
 
-except KeyboardInterrupt:
-    GPIO.cleanup()
-    print("GPIO 정리 완료")
+    except KeyboardInterrupt:
+        GPIO.cleanup()
+        print("GPIO 정리 완료")
+
+if __name__ == "__main__":
+    interface()
