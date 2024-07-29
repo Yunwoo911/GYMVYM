069aba2ac76b600617b81658e65071eaaefcef10
Gmin77 <gongminseung@gmail.com>
2024-07-30 04:20:36 +0900

    0730 profile_update & search add

--- config/settings.py
+++ config/settings.py
@@ -167,11 +167,3 @@ SESSION_EXPIRE_AT_BROWSER_CLOSE = True  # 브라우저를 닫으면 세션 만�
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = '/'
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
--- member_search/views.py
+++ member_search/views.py
@@ -1,15 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .serializers import CustomUserSearchSerializers
 from account.models import CustomUser
 from rest_framework.views import APIView
+import json
+from django.http import JsonResponse
+from rest_framework import status
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
 class SearchUserName(APIView):
     def get(self, request, username):
-        users = CustomUser.objects.filter(username=username)
+        users = CustomUser.objects.filter(username__icontains=username)
+        user_data = [{'username': user.username, 'email': user.email, 'nfc_uid': user.nfc_uid} for user in users]
+        return JsonResponse(user_data, safe=False, status=status.HTTP_200_OK)
 
-def search_memeber_view(request):
-    return render(request, "search_memeber.html")
+def search_member_view(request):
+    return render(request, "search/search_member.html")
 
 class nfc():
     pass
+
+# user테이블에 nfc_uid 등록
+@csrf_exempt
+def register_nfc(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        nfc_uid = request.POST.get('nfc_uid')
+
+        if not email or not nfc_uid:
+            return JsonResponse({'error': 'Missing email or NFC UID'}, status=400)
+        
+        try:
+            user = CustomUser.objects.get(email=email)
+            user.nfc_uid = nfc_uid
+            user.save()
+            # 성공적으로 저장한 후 검색 페이지로 리디렉션
+            return redirect('member_search:search_member')
+        except CustomUser.DoesNotExist:
+            return JsonResponse({'error': 'User not found'}, status=404)
+    
+    return JsonResponse({'message': 'Invalid request'}, status=400)
