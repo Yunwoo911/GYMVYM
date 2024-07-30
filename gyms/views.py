9de666f96bbf5392f0ef18dafe2e7d48a330ab65
NAMYUNWOO <ajtmxod000@naver.com>
2024-07-31 01:33:20 +0900

    마이그레이션 초기화

--- gyms/views.py
+++ gyms/views.py
@@ -128,12 +128,12 @@ def approve_trainer_request(request, request_id):
         return redirect('trainer_requests_list')  # 요청 리스트 페이지로 리디렉션
     return render(request, 'approve_trainer_request.html', {'trainer_request': trainer_request})
 
-# class TrainerRequestSuccessView(TemplateView):
-#     template_name = 'trainer_request_success.html'
+class TrainerRequestSuccessView(TemplateView):
+    template_name = 'trainer_request_success.html'
 
-# # 7/21 시작
-# class OwnerPageView(TemplateView):
-#     template_name = 'owner_page.html'
+# 7/21 시작
+class OwnerPageView(TemplateView):
+    template_name = 'owner_page.html'
 
 
 
