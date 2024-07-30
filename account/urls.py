b81b6a16e141d3e083e482ab380c0b9185613539
Gmin77 <gongminseung@gmail.com>
2024-07-31 00:43:15 +0900

    search branch

--- account/urls.py
+++ account/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RegisterView, LoginView, ProfileUpdateView, home_view, profile_view, check_email_duplicate, update_nfc_view, verify_email, send_verification_code, profile_update_view
+from .views import RegisterView, LoginView, ProfileUpdateView, home_view, profile_view, check_email_duplicate, verify_email, send_verification_code, profile_update_view
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
@@ -15,6 +15,5 @@ urlpatterns = [
     path('check-email/', check_email_duplicate, name='check_email_duplicate'),
     path('verify-email/<int:user_id>/', verify_email, name='verify_email'),
     path('send-verification-code/', send_verification_code, name='send_verification_code'),
-    path('update_nfc/', update_nfc_view, name='update_nfc'),
 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
