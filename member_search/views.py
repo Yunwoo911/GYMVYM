e86ee5ef377d6bee1488434c621d2843785cc803
Gmin77 <gongminseung@gmail.com>
2024-07-31 01:36:53 +0900

    0731

--- member_search/views.py
+++ member_search/views.py
@@ -1,11 +1,14 @@
-from django.shortcuts import render, redirect
-from .serializers import CustomUserSearchSerializers
-from account.models import CustomUser
 from rest_framework.views import APIView
-import json
-from django.http import JsonResponse
+from rest_framework.response import Response
 from rest_framework import status
+from account.models import CustomUser
+from django.shortcuts import render, redirect
 from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
+import json
+
+def search_member_view(request):
+    return render(request, "search/search_member.html")
 
 class SearchUserName(APIView):
     def get(self, request, username):
@@ -13,18 +16,12 @@ class SearchUserName(APIView):
         user_data = [{'username': user.username, 'email': user.email, 'nfc_uid': user.nfc_uid} for user in users]
         return JsonResponse(user_data, safe=False, status=status.HTTP_200_OK)
 
-def search_member_view(request):
-    return render(request, "search/search_member.html")
-
-class nfc():
-    pass
-
-# user테이블에 nfc_uid 등록
 @csrf_exempt
 def register_nfc(request):
     if request.method == 'POST':
-        email = request.POST.get('email')
-        nfc_uid = request.POST.get('nfc_uid')
+        data = json.loads(request.body)
+        email = data.get('email')
+        nfc_uid = data.get('nfc_uid')
 
         if not email or not nfc_uid:
             return JsonResponse({'error': 'Missing email or NFC UID'}, status=400)
@@ -33,8 +30,7 @@ def register_nfc(request):
             user = CustomUser.objects.get(email=email)
             user.nfc_uid = nfc_uid
             user.save()
-            # 성공적으로 저장한 후 검색 페이지로 리디렉션
-            return redirect('member_search:search_member')
+            return JsonResponse({'status': 'success'})
         except CustomUser.DoesNotExist:
             return JsonResponse({'error': 'User not found'}, status=404)
     
