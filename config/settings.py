342bea5163443fc49982c6933a37a724120a8c39
SinMoonGh <109773691+SinMoonGh@users.noreply.github.com>
2024-07-30 15:42:40 +0900

    done

--- config/settings.py
+++ config/settings.py
@@ -11,8 +11,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-from decouple import config
+from dotenv import load_dotenv
 import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
